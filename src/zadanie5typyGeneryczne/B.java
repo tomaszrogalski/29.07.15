a44de6b42e017f7f737d7d45b67f88d023d84aff
tomaszrogalski <aytar@o2.pl>
2015-07-29 13:01:57 +0200

    typyGeneryczne - klasy cd

--- src/zadanie5typyGeneryczne/B.java
+++ src/zadanie5typyGeneryczne/B.java
@@ -11,10 +11,10 @@ public class B<T extends A, S extends Number> {
 
 	public static void main(String[] args) {
 
-		B<A, Number> obiekt1 = new B<A, Number>(new A(), 3);// OK
-		B<A, Number> obiekt2 = new B<A, Number>(3, new A());// NIE OK
-		B<Number, A> obiekt3 = new B<Number, A>(3, new A());// NIE OK
+		B<A, Number> obiekt1 = new B<A, Number>(new A(), new Double(3.0));// OK
+		// B<A, Number> obiekt2 = new B<A, Number>(3, new A());// NIE OK
+		// B<Number, A> obiekt3 = new B<Number, A>(3, new A());// NIE OK
 		B obiekt4 = new B(new A(), 3);// OK
-		B obiekt5 = new B(3, new A());// NIE OK
+		// B obiekt5 = new B(3, new A());// NIE OK
 	}
 }
