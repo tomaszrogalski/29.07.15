bc8a76837bf7a7bc98b00862af9e78fc3bba1cb7
tomaszrogalski <aytar@o2.pl>
2015-07-29 09:07:59 +0200

    zadanie3

---
+++ src/zadanie3/A.java
@@ -0,0 +1,17 @@
+package zadanie3;
+
+public class A {
+
+	private int x;
+	
+	
+	public A(int x) {
+		super();
+		this.x = x;
+	}
+
+
+//	private A(){
+//		
+//	}
+}
---
+++ src/zadanie3/B.java
@@ -0,0 +1,10 @@
+package zadanie3;
+
+public class B extends A{
+
+	public B(int x) {
+		super(x);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
